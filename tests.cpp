36e5374beb75489e1130ff0ab387e93ed77b8156
anthonypoon12 <anthony@poons.us>
2021-10-14 10:33:02 -0400

    addede testcases for countChar

--- tests.cpp
+++ tests.cpp
@@ -9,3 +9,9 @@ TEST_CASE("removeLeadingSpaces")
     CHECK(removeLeadingSpaces("         hello2   ")=="hello2   ");
     CHECK(removeLeadingSpaces(" hello3		")=="hello3		");
 }
+TEST_CASE("countChar")
+{
+	CHECK(countChar("",'a')==0);
+	CHECK(countChar("a",'a')==1);
+	CHECK(countChar("hello",'l')==2);
+}

c362d007119e8517ecfd107cc34610d405447bbe
anthonypoon12 <anthony@poons.us>
2021-10-14 09:41:30 -0400

    added removeLeadingSpaces

--- funcs.cpp
+++ funcs.cpp
@@ -1,1 +1,21 @@
+/*
+ Task A: that takes one line of code as input and returns its copy without leading spaces and tabs
+*/
 #include <iostream>
+#include <cctype>
+
+std::string removeLeadingSpaces(std::string line)
+{
+    bool start=false;
+    std::string output="";
+    if (line=="")
+        return "";
+    for (char c : line)
+    {
+        if (start)
+            output+=c;
+        else if (isspace(c))
+            start=true;
+    }
+    return output;
+}

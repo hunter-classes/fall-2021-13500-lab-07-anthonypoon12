3512401d61910819f0293f7c3c770d8a16b2ada1
anthonypoon12 <anthony@poons.us>
2021-10-14 09:49:46 -0400

    added testcases for removeLeadingSpaces

--- tests.cpp
+++ tests.cpp
@@ -1,3 +1,11 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "funcs.h"
+
+TEST_CASE("removeLeadingSpaces")
+{
+    CHECK(removeLeadingSpaces("")=="");
+    CHECK(removeLeadingSpaces("         hello")=="hello");
+    CHECK(removeLeadingSpaces("         hello2   ")=="hello2   ");
+    CHECK(removeLeadingSpaces(" hello3      ")=="hello3        ");
+}

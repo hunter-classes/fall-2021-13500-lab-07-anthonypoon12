1d45f10782c5758298ccdd71e9cac3dc6d53a64b
anthonypoon12 <anthony@poons.us>
2021-10-19 21:56:44 -0400

    changed main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+#include <iostream>
+#include "funcs.h"
 int main()
 {
-    
+    ifstream file("bad-code.cpp");
+    std::cout<<unindent(file)<<"\n";
+    std::cout<<indent(file)<<"\n";
 }

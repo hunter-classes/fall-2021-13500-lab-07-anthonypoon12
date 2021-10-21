ae9c5cb4c279943a99b02999ebb808d44820fa14
anthonypoon12 <anthony@poons.us>
2021-10-20 22:32:43 -0400

    Added comments

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/*
+Author: Anthony Poon
+Course: CSCI-135
+Instructor: Tong Yi/Mike Zamansky
+Assignment: Lab 7
+Main file to run Lab 7
+*/
 #include <iostream>
 #include <fstream>
 #include "funcs.h"

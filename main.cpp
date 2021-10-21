7aa394c6e02c616019d142ee12525eda5f0bc49f
anthonypoon12 <anthony@poons.us>
2021-10-21 08:12:42 -0400

    changed main to make new files and added other bad code

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@
 int main()
 {
     std::string file ("bad-code.cpp");
-    std::cout<<unindent(file)<<"\n";
-//	std::cout<<"hello";
-    std::cout<<indent(file)<<"\n";
+	std::string file2 ("bad-code-2.cpp");
+    std::cout<<unindent(file,"unindentedbadcode.cpp")<<"\n";
+    std::cout<<indent(file,"indentedbadcode.cpp")<<"\n";
+	std::cout<<unindent(file2, "unindentedbadcode2.cpp")<<"\n";
+	std::cout<<indent(file2, "indentedbadcode2.cpp")<<"\n";
+	return 0;
 }

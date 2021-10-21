a1f683f6a0901159e9862ca359d629833917abd6
anthonypoon12 <anthony@poons.us>
2021-10-21 08:11:43 -0400

    added more bad code

---
+++ bad-code-2.cpp
@@ -0,0 +1,11 @@
+                int main(){
+           // Hi, I'm a program!
+														int x = 1; 
+    for(int i = 0; i < 10; i++) {
+cout << i;
+for (int i = 0; i<2;i++){
+std::cout<<"hi\n";
+}
+          cout << endl;
+ }
+    }
